6b8767ba34010fd3b74f8913f691dae94fa31800
Ives Costa <ivespauiniam@gmail.com>
2021-05-09 23:35:43 -0500

    create model core

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Venda, Item
+# Register your models here.
+admin.site.register(Venda)
+admin.site.register(Item)
---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Venda(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.IntegerField()
+    status = models.CharField(max_length=20, default='em processo')
+    def __str__(self):
+        return str(self.code) + "----" + str(self.owner) 
+
+class Item(models.Model):
+    nome_items= models.CharField(max_length=30)
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.nome_items)
